cdfeeab92644cabbc9e0990faa3e1d7e5ba3207e
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2023-11-08 11:38:44 +0600

    Added book borrow functionality

--- src/components/each-book/EachBook.jsx
+++ src/components/each-book/EachBook.jsx
@@ -2,70 +2,64 @@ import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
 import Rating from "react-rating";
 import { BsStar, BsStarFill } from "react-icons/bs";
+import Modal from "./each-book-comps/Modal";
 
 const EachBook = () => {
-    const location = useLocation()
-    console.log(location.search)
-    useEffect( () => {
-        axios.get(`http://localhost:4000/books${location.search}`)
-        .then(data => setBook(data.data[0]))
-        .catch(err => console.log(err))
-    } , [])
+  const location = useLocation();
+  console.log(location.search);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/books${location.search}`)
+      .then((data) => setBook(data.data[0]))
+      .catch((err) => console.log(err));
+  }, []);
 
-    const [ book, setBook ] = useState({})
+  const [book, setBook] = useState({});
 
-    const HandleRead = () => {}
-    const HandleBorrow = () => {}
+  const HandleRead = () => {};
 
-    return (
-        <div>
-        <div className="hero min-h-screen bg-base-200">
-          <div className="hero-content w-3/4 flex-col lg:flex-row gap-10">
-              <div className="bg-base-100 p-4 shadow-lg">
-              <img
-              src={book.Image}
-              className="rounded-lg h-72"
-            />
-              </div>
-            <div className="text-center md:text-left">
-              <h1 className="text-3xl font-semibold">{book.Name}</h1>
-              <h2 className="font-semibold">{book["Author Name"]}</h2>
-              <p className="py-1 text-sm text-gray-600">
-                  {book.Category}
-              </p>
-              <p className="mt-3">
-                <Rating
-                  initialRating={book.Rating}
-                  readonly
-                  className="text-xs"
-                  fullSymbol={<BsStarFill></BsStarFill>}
-                  emptySymbol={<BsStar></BsStar>}
-                ></Rating>
-              </p>
-              {/* <p className="mt-4 text-sm max-w-lg">
+
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content w-3/4 flex-col lg:flex-row gap-10">
+          <div className="bg-base-100 p-4 shadow-lg">
+            <img src={book.Image} className="rounded-lg h-72" />
+          </div>
+          <div className="text-center md:text-left">
+            <h1 className="text-3xl font-semibold">{book.Name}</h1>
+            <h2 className="font-semibold">{book["Author Name"]}</h2>
+            <p className="py-1 text-sm text-gray-600">{book.Category}</p>
+            <p className="">
+              <Rating
+                initialRating={book.Rating}
+                readonly
+                className="text-xs"
+                fullSymbol={<BsStarFill></BsStarFill>}
+                emptySymbol={<BsStar></BsStar>}
+              ></Rating>
+            </p>
+            {/* <p className="mt-4 text-sm max-w-lg">
                 {book["Short description"]}
               </p> */}
-              <p className="flex gap-5 justify-center">
-              <button className="lg:w-40 btn1 border-base-content"
-              onClick={HandleRead}>
-                 Read
+            <p className="flex gap-5 justify-center mt-4">
+              <button
+                className="lg:w-40 btn1 border-base-content"
+                onClick={HandleRead}
+              >
+                Read
               </button>
-              <button className="lg:w-40 btn1 border-base-content"
-              onClick={() => HandleBorrow(book._id)}>
-                  Borrow
-              </button>
-  
-              </p>
-              
-            </div>
+              <Modal book={book}></Modal>
+            </p>
           </div>
         </div>
-        <Toaster></Toaster>
       </div>
-    );
+      <Toaster></Toaster>
+    </div>
+  );
 };
 
 export default EachBook;
---
+++ src/components/each-book/each-book-comps/Modal.jsx
@@ -0,0 +1,104 @@
+/* eslint-disable react/prop-types */
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { AuthProvider } from "../../../../context/AuthContext";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
+
+const Modal = ({ book }) => {
+
+    let { Name, Quantity } = book
+
+    const { user } = useContext(AuthProvider)
+
+    const { register, handleSubmit } = useForm()
+
+    const handleBorrow = (d) => {
+        let userName = d.Name
+        let email = user.email
+        let returnDate = d.Date
+        axios.get(`http://localhost:4000/borrowedInfo?name=${userName}&book=${Name}`)
+        .then(data =>{
+            if(data.data[0]?.Name !== Name && data.data[0]?.email !== email){
+            axios.post(`http://localhost:4000/borrowedInfo?name=${user.Name}&book=${Name}`, {userName, email, Name, returnDate})
+            .then(data => toast("You have borrowed", data.data[0].Name))
+            .catch(err => console.log(err))
+        }
+        else{
+            toast("You have already borrowed this book")
+        }})
+        .catch(err => console.log(err))
+    }
+
+  return (
+    <div>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <button
+        className="lg:w-40 btn1 border-base-content"
+        onClick={() => document.getElementById("my_modal_1").showModal()}
+      >
+        Borrow
+      </button>
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box rounded-none">
+
+        <form className="card-body"
+          onSubmit={handleSubmit(handleBorrow)}>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  {...register("Name")}
+                  type="text"
+                  defaultValue={user.displayName}
+                  className="input input-bordered  rounded-none focus:outline-none"
+                  readOnly
+                  required
+                />
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  {...register("Email")}
+                  type="email"
+                  defaultValue={user.email}
+                  className="input input-bordered  rounded-none focus:outline-none"
+                  readOnly
+                  required
+                />
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Return Date</span>
+                </label>
+                <input
+                  {...register("Date")}
+                  type="date"
+                  className="input input-bordered  rounded-none focus:outline-none outline-none"
+                  required
+                />
+              </div>
+
+              <div className="modal-action">
+          <button className="btn1 border-base-content w-24">Submit</button>
+            <form method="dialog">
+              <button className="btn1 border-base-content w-24">Close</button>
+            </form>
+          </div>
+            
+        </form>
+
+        </div>
+      </dialog>
+     <Toaster></Toaster>
+    </div>
+  );
+};
+
+export default Modal;
